7c73e20d956f12942becbbe1b457680778b48d6a
Frank <55867157+thedraxx@users.noreply.github.com>
2022-08-01 16:11:52 -0300

    init design

---
+++ src/screens/CalculadoraScreen.tsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react'
+import { Text, View } from 'react-native'
+import { styles } from '../theme/AppTheme'
+export const CalculadoraScreen = () => {
+  return (
+        <View >
+            <Text style={styles.texto}>Hola Mundo</Text>
+        </View>
+  )
+}
---
+++ src/theme/AppTheme.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  fondo: {
+    flex: 1,
+    backgroundColor: '#000000'
+  },
+  texto: {
+    color: 'white',
+    fontSize: 30
+  }
+})
