6edd0b404b1c19ac56b36f8bd750605378d033a2
Frank <55867157+thedraxx@users.noreply.github.com>
2022-08-01 21:21:29 -0300

    text and first button

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -4,8 +4,16 @@ import { Text, View } from 'react-native'
 import { styles } from '../theme/AppTheme'
 export const CalculadoraScreen = () => {
   return (
-        <View >
-            <Text style={styles.texto}>Hola Mundo</Text>
+        <View style={styles.calculadoraContainer}>
+            <Text style={styles.resultadopequeno}> 1,500.00 </Text>
+            <Text style={styles.resultado}> 1,500.00 </Text>
+
+            <View>
+              {/* boton */}
+              <View style={styles.boton}>
+                <Text style={styles.botonTexto}>1</Text>
+              </View>
+            </View>
         </View>
   )
 }
--- src/theme/AppTheme.tsx
+++ src/theme/AppTheme.tsx
@@ -3,10 +3,43 @@ import { StyleSheet } from 'react-native'
 export const styles = StyleSheet.create({
   fondo: {
     flex: 1,
-    backgroundColor: '#000000'
+    backgroundColor: '#000000',
+    padding: 20
+
   },
-  texto: {
+  calculadoraContainer: {
+    paddingHorizontal: 20,
+    flex: 1,
+    justifyContent: 'flex-end'
+  },
+  resultado: {
     color: 'white',
-    fontSize: 30
+    fontSize: 60,
+    textAlign: 'right',
+    fontWeight: 'bold'
+
+  },
+  resultadopequeno: {
+    color: 'rgba(255,255,255,0.5)',
+    fontSize: 30,
+    textAlign: 'right',
+    fontWeight: 'bold'
+  },
+
+  boton: {
+    height: 80,
+    width: 80,
+    backgroundColor: '#9B9B9B',
+    borderRadius: 100,
+    justifyContent: 'center'
+  },
+
+  botonTexto: {
+    textAlign: 'center',
+    padding: 10,
+    fontSize: 30,
+    color: 'black',
+    fontWeight: 'bold'
   }
+
 })
